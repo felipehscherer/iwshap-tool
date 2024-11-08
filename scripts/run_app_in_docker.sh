561e40765213f540b7acda16f2b6ec6f8f8c976e
felipehscherer <felipe.scherepento@gmail.com>
2024-11-08 20:23:09 -0300

    refactor: refactor scripts

--- scripts/run_app_in_docker.sh
+++ scripts/run_app_in_docker.sh
@@ -3,7 +3,7 @@
 echo "Executando o IWSHAP.."
 echo ""
 
-python3 IWSHAP.py -s dataset/safe_dataset.csv -a dataset/attack_dataset_fabr.csv -x -n
+python3 IWSHAP.py -s your_dataset -a your_dataset -x -n
 
 echo "Processo finalizado!"
 
